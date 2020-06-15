f510c89c6521c2b5f3ea1d0bb41059a85b95d749
yash-dani <daniyash19@gmail.com>
2020-06-14 22:45:54 -0400

    experimenting with code blocks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,46 +5,14 @@ import {DOMParser, Schema} from "prosemirror-model"
 import {schema} from "prosemirror-schema-basic"
 //import {addListNodes} from "prosemirror-schema-list"
 //import {exampleSetup} from "prosemirror-example-setup"
-import './App.css';
-export collab from "prosemirror-collab"
-import {baseKeymap} from 'prosemirror-commands'
+//import './App.css';
+import CodeMirror from "codemirror"
+import {baseKeymap, exitCode} from 'prosemirror-commands'
 import {undo, redo, history} from "prosemirror-history"
 import {keymap} from "prosemirror-keymap"
 
-
-
 function App() {
     useEffect(() => {
-        class Authority {
-          constructor(doc) {
-            this.doc = doc
-            this.steps = []
-            this.stepClientIDs = []
-            this.onNewSteps = []
-          }
-
-          receiveSteps(version, steps, clientID) {
-            if (version != this.steps.length) return
-
-            // Apply and accumulate new steps
-            steps.forEach(step => {
-              this.doc = step.apply(this.doc).doc
-              this.steps.push(step)
-              this.stepClientIDs.push(clientID)
-            })
-            // Signal listeners
-            this.onNewSteps.forEach(function(f) { f() })
-          }
-
-          stepsSince(version) {
-            return {
-              steps: this.steps.slice(version),
-              clientIDs: this.stepClientIDs.slice(version)
-            }
-          }
-        }
-
-
         // Schemas
         const trivialSchema = new Schema({
           nodes: {
@@ -78,7 +46,7 @@ function App() {
         })
         // Document structure
         let doc = schema.node("doc", null, [
-          schema.node("paragraph", null, [schema.text("Hello, this is some sample text. Click here to start editing!")]),
+          schema.node("paragraph", null, [schema.text("Hello, test doc and testing and testing and testing!")]),
         ]);
 
 
@@ -92,33 +60,20 @@ function App() {
             keymap(baseKeymap)
           ]
         })
-        function collabEditor(authority, place) {
-          let view = new EditorView(place, {
-            state: EditorState.create({
-              doc: authority.doc,
-              plugins: [collab({version: authority.steps.length})]
-            }),
-            dispatchTransaction(transaction) {
-              let newState = view.state.apply(transaction)
-              view.updateState(newState)
-              let sendable = collab.sendableSteps(newState)
-              if (sendable)
-                authority.receiveSteps(sendable.version, sendable.steps,
-                                       sendable.clientID)
-            }
-          })
 
-          authority.onNewSteps.push(function() {
-            let newData = authority.stepsSince(collab.getVersion(view.state))
-            view.dispatch(
-              collab.receiveTransaction(view.state, newData.steps, newData.clientIDs))
-          })
-
-          return view
-        }
         // View
-        var a = new Authority(doc)
-        let view = collabEditor(a, document.body)
+        let view = new EditorView(document.body, {
+          state,
+          dispatchTransaction(transaction) {
+            console.log("Document size went from", transaction.before.content.size,
+                        "to", transaction.doc.content.size)
+            //transaction.split(2)
+            let newState = view.state.apply(transaction)
+            
+
+            view.updateState(newState)
+          }
+        })
         /*const mySchema = new Schema({
             nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
             marks: schema.spec.marks
@@ -138,7 +93,7 @@ function App() {
         <h1 id="content">Real Time Text Editor</h1>
         <p>Made with ❤️ by Yash Dani, June 2020.</p>     
         <br />
-        <h2>The Text Editor</h2>          
+        <p>Text Editor</p>          
         <div id="editor" />
         <div id="content" />
 
